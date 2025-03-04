e0dfdc44abcfa0ff1212e7abc1d5eb3443866e6c
ElizavetaShikhova <elizaveta.liza06@mail.ru>
2025-03-04 18:04:02 +0500

    show command

--- index.js
+++ index.js
@@ -11,10 +11,37 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function extractTODO(){
+    let comments = [];
+    for (const fileContent of files) {
+        const lines = fileContent.split('\n');
+        for (const line of lines) {
+            if (line.includes("// TODO")){
+                comments.push(line.split("// TODO")[1].trim());
+            };
+        }
+    }
+    return comments;
+}
+
+function exctractImportant(comments){
+    let important = [];
+    for (const comment of comments){
+        if (comment.includes("!")){
+            important.push(comment);
+        }
+    }
+    return important;
+}
+
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(extractTODO());
             break;
         default:
             console.log('wrong command');
